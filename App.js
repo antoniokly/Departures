69094e48e425372208ddc02b09839e42784f49f6
Antonio Yip <DEVAntonio.yip@asb.co.nz>
2019-09-07 23:15:00 +1200

    styling

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Picker, SafeAreaView, Button} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import DateTimePicker from "react-native-modal-datetime-picker";
 import SegmentedControlTab from "react-native-segmented-control-tab";
+import { Button } from 'react-native-elements';
+
 
 const moment = require('moment');
 
@@ -68,11 +70,13 @@ export default class App extends Component {
           <Text style={styles.title}>Departures</Text>
         </View>
         
-        <View style={styles.itemContainer}>
+        <View style={styles.top}>
           <Text style={styles.itemName}>Type</Text>
           <View style={styles.picker}>
             <SegmentedControlTab
               style={styles.picker}
+              tabStyle={styles.tabStyle}
+              tabTextStyle={styles.buttonTextStyle}
               values={["Any", "Train", "Bus"]}
               selectedIndex={this.state.type + 1}
               onTabPress={this.handleIndexChange}
@@ -83,18 +87,52 @@ export default class App extends Component {
         <View style={styles.itemContainer}>
           <Text style={styles.itemName}>From:</Text>
           <Text style={styles.label}>{this.fromDateString()}</Text>
-          <Button title="Change" onPress={this.showFromDateTimePicker} />
-            
+          <View style={styles.button}>
+            <Button
+              icon={{
+                name: "today",
+                size: 20,
+                color: "gray"
+              }}
+              type="outline"
+              title="Change" 
+              onPress={this.showFromDateTimePicker} 
+              titleStyle={styles.buttonTextStyle}
+
+            />
+          </View>
         </View>
 
         <View style={styles.itemContainer}>
           <Text style={styles.itemName}>To:</Text>
           <Text style={styles.label}>{this.toDateString()} </Text>
-          <Button title="Change" onPress={this.showToDateTimePicker} />
+          <View style={styles.button}>
+            <Button
+              icon={{
+                name: "today",
+                size: 20,
+                color: "gray"
+              }}            
+              type="outline"
+              title="Change" 
+              onPress={this.showToDateTimePicker} 
+              titleStyle={styles.buttonTextStyle}
+            />
+          </View>
         </View>
 
-        <View style={styles.itemContainer}>
-          <Button title="Search" onPress={this.search} />
+        <View style={styles.bottom}>
+          <Button
+              icon={{
+                name: "search",
+                size: 20,
+                color: "gray"
+              }}  
+              type="outline"
+              title="Search" 
+              onPress={this.search} 
+              titleStyle={{fontSize: 16}}
+            />
         </View>
 
         <DateTimePicker
@@ -114,22 +152,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-    flexDirection: 'column'
-
-  },
-  gridView: {
-    marginTop: 10,
-    width: '100%',
-    height: '100%',
+    flexDirection: 'column',
+    padding: 20,
+    width: '100%'
   },
   itemContainer: {
+    flex: 1,
     width: '100%',
     borderRadius: 5,
-    padding: 20,
     alignItems: 'center',
   },
   picker: {
-    width: '100%',
+    flex: 1,
+    width: '80%',
     borderRadius: 5,
     padding: 10,
   },
@@ -139,42 +174,14 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 18,
-    color: "gray"
-  },
-  sectionHeader: {
-    flex: 1,
-    fontSize: 15,
-    fontWeight: '600',
-    alignItems: 'center',
-    backgroundColor: '#636e72',
-    color: 'white',
-    padding: 10,
-  },
-  image: {
-    resizeMode: 'cover',
-    flex: 1, 
-  },
-  button:{
-    width: 150,
-    marginTop:10,
-    paddingTop:10,
-    paddingBottom:10,
-    backgroundColor:'#68a0cf',
-    borderRadius:10,
-    borderWidth: 1,
-    borderColor: '#fff'
+    color: "gray",
+    padding: 5,
   },
   contentView:{
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
     height: '100%',
-  },
-  buttonText:{
-      color:'#fff',
-      textAlign:'center',
-      fontSize: 18,
-      fontWeight: '600',
   },
   appHeader: {
     alignItems: 'center',
@@ -186,5 +193,28 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: '600',
     color: 'black',
-  }
+  },
+  button: {
+    flex: 1,
+    width: '40%',
+  },
+  top: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 36,
+  },
+  bottom: {
+    flex: 1,
+    width: '40%',
+    justifyContent: 'flex-end',
+    marginBottom: 36
+  },
+  tabStyle: {
+    backgroundColor: "#F5FCFF"
+  },
+  buttonTextStyle: {
+    fontSize: 16
+  },
 });
